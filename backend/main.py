da8c01f58119568bddaca18442b8b517109f0b3d
ndl1n <a0938391667@gmail.com>
2025-08-23 22:10:16 +0800

    add CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from api.v1 import auth # 匯入 auth 路由模組
+from fastapi.middleware.cors import CORSMiddleware
 from core.database import engine, Base  # 你的 Base 和 engine
 
 app = FastAPI(title="Skill Map API")
+
+# 允許跨來源請求
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允許所有來源，開發方便（正式環境建議改成前端網址）
+    allow_credentials=True,
+    allow_methods=["*"],  # 允許所有方法 (GET, POST, PUT, DELETE, OPTIONS...)
+    allow_headers=["*"],  # 允許所有 headers
+)
 
 # 掛載 v1 版本的 auth 路由，並設定前綴
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
